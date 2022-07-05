fa19e066769a9998b3e455c3e918d44d6839816d
Joshua Santiago <joshua.santiago14@upr.edu>
2022-07-04 21:39:02 -0400

    mod

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import os
-
 import requests
 import json
 import asyncio
@@ -12,73 +7,78 @@ from time import gmtime, strftime, sleep
 import pandas as pd
 
 
+def getDir():
+    savefile = "./OBA_DATABASE/" + str(date.today().strftime("%Y-%m")) + '/' + str(date.today().strftime("%d"))
 
-def getVehicles(name):
-    # Use a breakpoint in the code line below to debug your script.
-    filename = str(date.today()) + '-TRACCAR.csv'
-    # if os.path.isfile('/home/' + os.getlogin() + '/OBA_DATABASE/' + str(date.today()) +'/'+ filename ):
-    #     print('Found record: ' + filename)
-    # else:
-    #     print('No record found: ' + filename)
-    #     create_folder(os.makedirs('/home/' + os.getlogin() + '/OBA_DATABASE/' + str(date.today())))
-    #     print('Created record: ' + filename)
+    if os.path.isdir("./OBA_DATABASE"):
+        print('./OBA_DATABASE already exists')
+    else:
+        os.makedirs("./OBA_DATABASE")
+        os.makedirs("./OBA_DATABASE/" + str(date.today().strftime("%Y-%m")))
+        os.makedirs("./OBA_DATABASE/" + str(date.today().strftime("%Y-%m")) + '/' + str(date.today().strftime("%d")))
+        sleep(2)
+    return savefile
 
+
+def getLocs(dir):
     traccarrequest = requests.get(
         'http://167.172.12.82:8080/api/v1/key/f78a2e9a/agency/1/command/vehicles?r=%20&format=json')
     response = json.loads(traccarrequest.text)
     response = response["vehicles"]
     fetched = []
     timestamp = str(datetime.now())
-    print(response)
 
     for item in response:
         fetched.append(
             [item['id'], item['vehicleType'], item['loc']['lat'], item['loc']['lon'], item['loc']['time'], timestamp])
 
+    filedir = os.path.join(dir, str(date.today().strftime("%d")) + '-TRACCAR.csv')
+    temp = None
+    if os.path.isfile(filedir):
+        print(filedir)
+        temp = pd.read_csv(filedir)
     dfTraccar = pd.DataFrame(fetched, columns=['id', 'vehicleType', 'lon', 'lat', 'time', 'timestamp'])
-    dfTraccar.to_csv(filename)
+    if temp is None:
+        dfTraccar.to_csv(filedir)
+    else:
+        val = pd.concat([temp, dfTraccar])
+        val.to_csv(filedir, index=False,
+                   columns=['id', 'vehicleType', 'lon', 'lat', 'time', 'timestamp'])
 
 
-def getRoutes(name):
-    # Use a breakpoint in the code line below to debug your script.
-    filename = str(date.today()) + '-OBA.csv'
-
-    traccarrequest = requests.get('http://167.172.12.82:8080/api/v1/key/f78a2e9a/agency/1/command/gtfs-rt/vehiclePositions?format=human')
-    response = traccarrequest.text
-    # fetched = []
-    data = response[response.find("}"):]
-    newdata = data
-    for x in range(data.count('entity')):
-        newdata = data[data.find('entity')]
+def getRoutes(dir):
+    req = requests.get(
+        'http://167.172.12.82:8080/api/v1/key/f78a2e9a/agency/1/command/gtfs-rt/tripUpdates?format=human')
+    data = str(req.content)
+    data = data.split('\\n')
 
 
 
-    # timestamp = strr(datetime.now())
-    #
-    # for item in response:
-    #     fetched.append(
-    #         [item['id'], item['vehicleType'], item['loc']['lat'], item['loc']['lon'], item['loc']['time'], timestamp])
-    #
-    # dfTraccar = pd.DataFrame(stmod)
-    # print(dfTraccar)
-    # dfTraccar.to_csv(filename)
+def getVehicles(dir):
+    req = requests.get(
+        'http://167.172.12.82:8080/api/v1/key/f78a2e9a/agency/1/command/gtfs-rt/vehiclePositions?format=human')
+    data = req.text
+    data = data.split('\n')
 
+    keywords = ['id', 'latitude', 'longitude']
 
-def setDir(str):
-    print(str)
-    os.chdir('/home/' + os.getlogin() + '/OBA_DATABASE/' + str(date.today()))
-    pass
+    df = pd.DataFrame()
 
+    for x in range(0, len(data)):
+        print(data[x])
+        for word in keywords:
+            if word in data[x]:
+                df[word].append(data[x].split(':')[1])
+    print(df)
 
 if __name__ == '__main__':
-    path = os.getcwd(   )
 
-    sec = 15
+    sec = 20
+    savefile = getDir()
+
     for tick in range(0, sec):
-        setDir("The current working directory is %s" % path)
-        getVehicles('Vehicles ...')
-        getRoutes('Routes ...')
-        # asyncio.run(runner())
-        sleep(1)
+        getLocs(savefile)
+        getRoutes(savefile)
+        getVehicles(savefile)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        sleep(1)
